9902dcd5fe4984e49090e3b18926c4ec2097c12a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-07 17:44:00 -0500

    Prompt written

--- CPP-PRG-11-04-Weather-Statistics/main.cpp
+++ CPP-PRG-11-04-Weather-Statistics/main.cpp
@@ -5,11 +5,31 @@
 //  Created by Keith Smith on 11/6/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that uses a structure to store the following weather data for a
+//  particular month:
+//
+//      Total Rainfall
+//      High Temperature
+//      Low Temperature
+//      Average Temperature
+//
+//  The program should have an array of 12 structures to hold weather data for an entire
+//  year. When the program runs, it should ask the user to enter data for each month.
+//  (The average temperature should be calculated.) Once the data are entered for all the
+//  months, the program should calculate and display the average monthly rainfall, the
+//  total rainfall for the year, the highest and lowest temperatures for the year (and the
+//  months they occurred in), and the average of all the monthly temperatures.
+//
+//  Input Validation: Only accept temperatures within the range between -100 and +140
+//  degrees Fahrenheit.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+
     return 0;
 }
